d7030121d3028b8f13ec6e25b8920184cb4d6a60
Rasmus Kjeldgaard <rasmus@Rasmuss-MBP.wired.guardiam>
2016-05-15 15:44:32 +0200

    Rotation of plane working on both axes, but probably needs refactoring

---
+++ Assets/TiltFirstAxis.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TiltFirstAxis : MonoBehaviour {
+
+	public GameObject plane;
+
+	// Use this for initialization
+	void Start () {
+		plane = GameObject.Find ("GameArea");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Debug.Log (plane.transform.position);
+
+		float zPos = this.transform.position.z;
+		float xAngle = plane.transform.eulerAngles.x;
+
+		Debug.Log("zPos: " + zPos);
+		Debug.Log("Plane angles: " + xAngle);
+
+
+		float scaledPos = -(zPos/20);
+
+		//Debug.Log (xPos);
+		//Debug.Log (scaledPos);
+
+		if(zPos > 1 && (xAngle < 9 || xAngle > 352)){
+			plane.transform.Rotate (scaledPos, 0, 0);
+		}
+
+		if(zPos < -1 && (xAngle < 8 || xAngle > 351)){
+			plane.transform.Rotate (scaledPos, 0, 0);
+		}
+	}
+}
---
+++ Assets/TiltSecondAxis.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TiltSecondAxis : MonoBehaviour {
+	
+	public GameObject plane;
+
+	// Use this for initialization
+	void Start () {
+		plane = GameObject.Find ("GameArea");
+	}
+
+	// Update is called once per frame
+	void Update () {
+		//Debug.Log (plane.transform.position);
+
+		float xPos = this.transform.position.x;
+		float zAngle = plane.transform.eulerAngles.z;
+
+		Debug.Log("xPos: " + xPos);
+		Debug.Log("Plane angles: " + zAngle);
+
+
+		float scaledPos = -(xPos/20);
+
+		//Debug.Log (xPos);
+		//Debug.Log (scaledPos);
+
+		if(xPos > 1 && (zAngle < 9 || zAngle > 352)){
+			plane.transform.Rotate (0, 0, scaledPos);
+		}
+
+		if(xPos < -1 && (zAngle < 8 || zAngle > 351)){
+			plane.transform.Rotate (0, 0, scaledPos);
+		}
+	}
+}
