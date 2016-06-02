21709dbc326637d8baa78ae0d9c360827580e177
Rasmus Kjeldgaard <rkjeldgaard@post.au.dk>
2016-06-02 12:50:14 +0200

    The receiver now has a setter method that changes the ball size of both spheres, when a string of numbers is sent to the corresponding osc address

--- Assets/OSCReceiver.js
+++ Assets/OSCReceiver.js
@@ -1,17 +1,24 @@
 private var RemoteIP : String = "127.0.0.1";
 private var SendToPort : int = 3334;
+private var sizeSphere1 : String;
 public var ListenerPort : int = 1234;
+
+//public var controller : GameObject;
+var sphereScript : SphereSizeController;
 
 private var handler : Osc;
 
 public function Start () {
+
+	sphereScript = GameObject.Find("SphereSizeControl").GetComponent("SphereSizeController"); 
+
 	var udp : UDPPacketIO = GetComponent("UDPPacketIO");
 	udp.init(RemoteIP, SendToPort, ListenerPort);
 	handler = GetComponent("Osc");
 	handler.init(udp);
 	
 	// Examples
-	handler.SetAddressHandler("/sayhello", UpdateSphere1);
+	handler.SetAddressHandler("/sphere1", UpdateSphere1);
 	handler.SetAddressHandler("/sphere2", UpdateSphere2);
 }
 
@@ -19,11 +26,14 @@ function Update () {
 }	
 
 public function UpdateSphere1(oscMessage : OscMessage) : void {
-	var pressureSphere1 = oscMessage.Values[0];
-	Debug.Log("Sphere1");
+	sizeSphere1 = oscMessage.Values[0];
+	sphereScript.setSizeSphere1(sizeSphere1);
+	Debug.Log(sizeSphere1);
 }
 
 public function UpdateSphere2(oscMessage : OscMessage) : void {
-	var pressureSphere2 = oscMessage.Values[0];
-	Debug.Log("Sphere2");
+	var sizeSphere2 = oscMessage.Values[0];
+	sphereScript.setSizeSphere1(sizeSphere2);
+
+	Debug.Log(sizeSphere2);
 }
---
+++ Assets/plugins/SphereSizeController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SphereSizeController : MonoBehaviour {
+
+	public GameObject sphere1;
+	public GameObject sphere2;
+	private float sizeSphere1 = 1.5F;
+	private float sizeSphere2 = 1.5F;
+
+	// Use this for initialization
+	void Start () {
+		sphere1 = GameObject.Find("Sphere1");
+		sphere2 = GameObject.Find("Sphere2");
+	}
+		
+	// Update is called once per frame
+	void Update () {
+		sphere1.transform.localScale = new Vector3 (sizeSphere1, sizeSphere1, sizeSphere1);
+		sphere2.transform.localScale = new Vector3 (sizeSphere2, sizeSphere2, sizeSphere2);
+	}
+
+	public void setSizeSphere1(string stringSizeSphere1) {
+		sizeSphere1 = float.Parse (stringSizeSphere1);
+		Debug.Log (sizeSphere1);
+	}
+
+	public void setSizeSphere2(string stringSizeSphere2) {
+		sizeSphere2 = float.Parse (stringSizeSphere2);
+	}
+}
